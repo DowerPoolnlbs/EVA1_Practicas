59cfd42726a891f4761c1261f948dfa185d7437b
DowerPoolnlbs <59983960+DowerPoolnlbs@users.noreply.github.com>
2024-02-12 15:53:11 -0700

    Add files via upload

---
+++ EXA1_2_IDENTIFICADORES/src/main/java/com/mycompany/exa1_2_identificadores/EXA1_2_IDENTIFICADORES.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exa1_2_identificadores;
+
+/**
+ *
+ * @author DOWER
+ */
+public class EXA1_2_IDENTIFICADORES {
+
+    public static void main(String[] args) {
+        //variables:
+        //TIPO DE DATO + NOMBRE DEL IDENTIFICADOR:
+        //short int byte long --> enteros
+        //float double --> numeros con decimal
+        //char --> character --> caracteres
+        //boolean --> verdadero o falso
+        //String --> cadenas de una variable
+        int edad;//declaracion de una variable
+        String nombre;
+        double salario;
+        
+        //ASIGNACION DE VALORES:
+        //IDENTIFICADOR = VALOR:
+        //se coloca el valor 50 dentro de la variable edad
+        edad = 50;
+        nombre = "Juan Perez Jolote";
+        salario = 123.56;
+        //COMO LOS USAMOS:
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(salario);
+    }
+}
