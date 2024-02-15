35fcf1fd4eb8bd9695305c58b92a36bf5193281c
DowerPoolnlbs <59983960+DowerPoolnlbs@users.noreply.github.com>
2024-02-15 15:48:09 -0700

    Add files via upload

---
+++ EVA1_4_ASIGNACION/src/main/java/com/mycompany/eva1_4_asignacion/EVA1_4_ASIGNACION.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_4_asignacion;
+
+/**
+ *
+ * @author DOWER
+ */
+public class EVA1_4_ASIGNACION {
+
+    public static void main(String[] args) {
+        String nombre = "Andres Ivan Campos Carrillo";
+        int edad;//DECLARACION EJEMPLO
+        String carrera = "Ing. Sistemas Comoutacionales";
+        
+        edad = 18;//ASIGNACION --> UN SOLO = SIGNIFICA ASIGNAR EJEMPLO
+        System.out.println("Tu nombre es ("+nombre+")");
+        System.out.println("Tu edad es ("+edad+")");
+        System.out.println("Tu carrera es ("+carrera+")");
+    }
+}
